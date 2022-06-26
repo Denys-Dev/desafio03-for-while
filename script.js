c10b99a381eed2ab52ee0b1354647522582ca984
Denys-Dev <mrdenys18@gmail.com>
2022-06-26 17:34:01 -0300

    Desafio3-for-while

---
+++ script.js
@@ -0,0 +1,21 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+let number = 10
+
+while(number >= 1){
+    console.log(number);
+    number--
+}
+
+for (number = 1; number <= 100; number+=2){
+    console.log(number);
+}
+
+for (number = 0; number <= 100; number+=2){
+    console.log(number);
+}
+
+
+
